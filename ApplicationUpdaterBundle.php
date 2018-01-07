09ad73e982c5d73c047c3f50e491eb058592ee06
Michael Zapf <m.zapf@mztx.de>
2018-01-07 12:11:40 +0100

    add basic composer.json; add bundle class

---
+++ ApplicationUpdaterBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace mztx\ApplicationUpdaterBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author   :  Michael Zapf <m.zapf@mztx.de>
+ * @date     :  07.01.18
+ * @time     :  11:35
+ */
+class ApplicationUpdaterBundle extends Bundle
+{
+}
